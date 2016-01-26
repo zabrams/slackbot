abcd7f0a3f528029cf0c0b45756d67f1de524a40
Zach Abrams <zach@Zachs-MacBook.local>
2016-01-26 12:19:45 -0800

    fix news module

--- news.rb
+++ news.rb
@@ -3,14 +3,14 @@ module News
 	def News.fetch_news(slack_response)
 		case slack_response[0]
 		    when 'hacker-news'
-		      resp = get_json("https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty")
+		      resp = News.get_json("https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty")
 		      resp = resp[0..9]
 		      message = "Here are the top 10 HN stories: \n"
 		      
 		      resp.each do |story_id|
 		        story_url = "https://hacker-news.firebaseio.com/v0/item/#{story_id}.json?print=pretty"
-		        story_response = get_json(story_url)
-		        message += create_headlines(story_response)
+		        story_response = News.get_json(story_url)
+		        message += News.create_headlines(story_response)
 		      end
 		    when 'stock'
 		      resp = HTTParty.get('http://dev.markitondemand.com/Api/v2/Quote', :query => {:symbol => "#{slack_response[1]}"})
@@ -33,20 +33,20 @@ module News
 		      end
 
 		      if slack_response[1] == "popular"
-		        resp = get_json("http://api.nytimes.com/svc/mostpopular/v2/mostviewed/all-sections/1.json?api-key=77c81381526472f019114e6da8e2a40f:14:61565219")
+		        resp = News.get_json("http://api.nytimes.com/svc/mostpopular/v2/mostviewed/all-sections/1.json?api-key=77c81381526472f019114e6da8e2a40f:14:61565219")
 		        resp = resp['results']
 		        message = "Most popular stories from the NYT: \n"
 
 		        resp.each do |story|
-		          message += create_headlines(story)
+		          message += News.create_headlines(story)
 		        end
 		      else  
-		        if resp = get_json("http://api.nytimes.com/svc/topstories/v1/#{slack_response[1]}.json?api-key=3e34bef4efc68cca88cb6b727c4beb6d:14:61565219") 
+		        if resp = News.get_json("http://api.nytimes.com/svc/topstories/v1/#{slack_response[1]}.json?api-key=3e34bef4efc68cca88cb6b727c4beb6d:14:61565219") 
 		          resp = resp['results']
 		          message = "Top stories from the NYT #{slack_response[1]}: \n"
 
 		          resp.each do |story|
-		            message += create_headlines(story)
+		            message += News.create_headlines(story)
 		          end
 		        else
 		          message = "I support to below nyt commands: \n"+
@@ -81,7 +81,7 @@ module News
   		return message
 	end
 
-	def get_json(url)
+	def News.get_json(url)
   		response = HTTParty.get(url)
   		if response.parsed_response.first[0] == "Error"
     		return false
@@ -91,7 +91,7 @@ module News
   		end
 	end
 
-	def create_headlines(hash)
+	def News.create_headlines(hash)
   		message = "*#{hash["title"]}*, #{hash["url"]} \n"
 	end
   
